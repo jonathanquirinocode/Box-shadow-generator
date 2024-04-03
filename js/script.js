62328004fbebc35aa6f031fbfc57ff7824a7c7cf
Jonathan Quirino <jonathanquirino30@gmail.com>
2024-04-03 12:12:49 -0300

    Estrutura inicial do JavaScript
    Criação do construtor e seleção dos elementos

---
+++ js/script.js
@@ -0,0 +1,44 @@
+class BoxShadowGenerator {
+    constructor(
+        horizontal,
+        horizontalValue,
+        vertical,
+        verticalValue,
+        blur,
+        blurValue,
+        spread,
+        spreadValue,
+        previewBox,
+        rule,
+        webkitRule,
+        mozRule
+    ){
+        this.horizontal = horizontal;
+        this.horizontalValue = horizontalValue;
+        this.vertical = vertical;
+        this.verticalValue = verticalValue;
+        this.blur = blur;
+        this.blurValue = blurValue;
+        this.spread = spread;
+        this.spreadValue = spreadValue;
+        this.previewBox = previewBox;
+        this.rule = rule;
+        this.webkitRule = webkitRule;
+        this.mozRule = mozRule;
+    };
+};
+
+const horizontal = document.querySelector("#horizontal");
+const horizontalValue = document.querySelector("#horizontal-value");
+const vertical = document.querySelector("#vertical");
+const verticalValue = document.querySelector("#vertical-value");
+const blur = document.querySelector("#blur");
+const blurValue = document.querySelector("#blur-value");
+const spread = document.querySelector("#spread");
+const spreadValue = document.querySelector("#spread-value");
+
+const previewBox = document.querySelector("#box");
+
+const rule = document.querySelector("#rule");
+const webkitRule = document.querySelector("#webkit-rule");
+const mozRule = document.querySelector("#moz-rule");
